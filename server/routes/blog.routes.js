f3a84f6758ac2666d50a5c6d06bb8d944dc6edc4
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-07-26 09:43:19 +0530

    get all blogs

--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,12 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
 import multer from 'multer';
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 import { submitForm } from '../controllers/createPost.controllers.js';
-import { getPosts } from '../controllers/getPosts.controllers.js'
+import { getPosts } from '../controllers/getPosts.controllers.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
+// Apply Clerk authentication middleware
 router.post('/submit-form', upload.single('image'), submitForm);
-router.get('/posts', getPosts)
+router.get('/blogs', getPosts);
 
 export default router;
