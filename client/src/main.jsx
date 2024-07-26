fc341d4c4c4471e9ce9b5a608331a0c936f57a42
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-07-26 09:40:24 +0530

    clerkprovider proper implementation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './app/store.js'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { useNavigate } from 'react-router-dom'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -26,12 +25,12 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </ClerkProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 )
