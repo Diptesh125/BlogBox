05a406f36cf4e1297acc0ec33bc1583a0c3160cc
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-08-08 12:19:16 +0530

    clerk publishable key problem fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,21 +7,11 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './app/store.js'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = 'pk_test_YW11c2VkLWdhcmZpc2gtODYuY2xlcmsuYWNjb3VudHMuZGV2JA'
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
-// const ClerkWithRoutes = () => {
-//   const navigate = useNavigate()
-
-//   return <ClerkProvider
-//     publishableKey={PUBLISHABLE_KEY}
-//     navigate={(to) => navigate(to)}
-//   >
-
-//   </ClerkProvider>
-// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
